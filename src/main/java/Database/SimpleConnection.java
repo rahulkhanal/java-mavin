df9cf0dbb4deea610157ee6eaeeab1ae0f647aea
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2024-06-10 09:21:27 +0545

    database connection seperateion

--- src/main/java/Database/SimpleConnection.java
+++ src/main/java/Database/SimpleConnection.java
@@ -2,22 +2,25 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SimpleConnection {
-    private static String databaseURL = "jdbc:mysql://localhost:3306/college";
-    private  static String databaseUser = "root";
-    private  static String databasePassword = "";
+    private final static String databaseURL = "jdbc:mysql://localhost:3306/college";
+    private final static String databaseUser = "root";
+    private final static String databasePassword = "";
 
-    public static void main(String[] args) {
-        try{
-            Connection con = DriverManager.getConnection(databaseURL,databaseUser,databasePassword);
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(databaseURL, databaseUser, databasePassword);
             if (con != null) {
                 System.out.println("Successfully connected to database!");
             } else {
                 System.out.println("Connection Failed! Check output console");
             }
-        }catch(Exception e){
-            System.out.println(e.getMessage())  ;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+        return con;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
+import Database.SimpleConnection;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleConnection simpleConnection = new SimpleConnection();
+        SimpleConnection.getConnection();
         System.out.println("Hello world!");
     }
 }
